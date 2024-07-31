74110886bab51d1ec8230640262b3f50fd180034
VUDUMULA NAGA SAI RAHUL <110412514+Rahul21sai@users.noreply.github.com>
2024-07-31 15:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (42.53%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+      if(n == 0 || n == 1){
+        return 1;
+      }
+    //   int left = climbStairs(n-1);
+    //   int right = climbStairs(n-2);
+
+    //   return left + right; 
+    int[] dp = new int[n+1];
+    dp[0] = dp[1] = 1;
+
+    for(int i = 2; i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+    }
+}
