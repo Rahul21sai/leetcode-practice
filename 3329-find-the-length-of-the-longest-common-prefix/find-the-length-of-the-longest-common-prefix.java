f3ddcbdc2807fc281e37dbf606ac44a3f222d216
VUDUMULA NAGA SAI RAHUL <110412514+Rahul21sai@users.noreply.github.com>
2026-05-21 22:01:32 +0530

    Time: 73 ms (47.11%) | Memory: 81.7 MB (52.75%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,33 +1,46 @@
-import java.util.HashMap;
-
 class Solution {
+    public int digits(int x) {
+        int cnt = 0;
+        while(x > 0) {
+            cnt++;
+            x /= 10;
+        }
+        return cnt;
+    }
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashMap<String, Integer> prefixMap = new HashMap<>();
-        
-        // Step 1: Build the prefix map for arr1
-        for (int num : arr1) {
-            String strNum = Integer.toString(num);
-            String prefix = "";
-            for (char ch : strNum.toCharArray()) {
-                prefix += ch;
-                prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+        HashSet<Integer> prefixes =new HashSet<>();
+
+        // storing all prefixes of arr1
+        for(int num : arr1) {
+            int x = num;
+            while(x > 0) {
+                prefixes.add(x);
+                x /= 10;
             }
         }
-        
-        int maxLength = 0;
-        
-        // Step 2: Check for common prefixes in arr2
-        for (int num : arr2) {
-            String strNum = Integer.toString(num);
-            String prefix = "";
-            for (char ch : strNum.toCharArray()) {
-                prefix += ch;
-                if (prefixMap.containsKey(prefix)) {
-                    maxLength = Math.max(maxLength, prefix.length());
+
+        int ans = 0;
+
+        // check prefixes of arr2 numbers
+        for(int num : arr2) {
+            int x = num;
+            int len = digits(num);
+
+            // checking from larger => smaller
+            while(x > 0) {
+                if(prefixes.contains(x)) {
+                    ans = Math.max(ans, len);
+                    // first match is the longest
+                    // so we stop
+                    break;
                 }
+
+                x /= 10;
+                len--;
             }
         }
-        
-        return maxLength;
+
+        return ans;
     }
 }
